53ac4f2290ba83381223b397ed5d5b2c3ebe762e
Kokichi516 <1209896981@qq.com>
2019-09-25 19:55:53 +0800

    first commit

---
+++ MyMaven1/src/main/java/com/hunau/test/App.java
@@ -0,0 +1,20 @@
+package com.hunau.test;
+
+/**
+ * @param $params$
+ * @Description:
+ * @Return: $returns$
+ * @开发人员:吉婷瑄
+ * @开发单位：湖南农业大学物联网工程专业
+ * @Date: $date$ $time$
+ * @开放版本：综合练习v0.1
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+}
---
+++ myboot/src/main/java/com/hunau/myboot/controller/GreetingController.java
@@ -0,0 +1,13 @@
+package com.hunau.myboot.controller;
+
+/**
+ * @param $params$
+ * @Description:
+ * @Return: $returns$
+ * @开发人员:吉婷瑄
+ * @开发单位：湖南农业大学物联网工程专业
+ * @Date: $date$ $time$
+ * @开放版本：综合练习v0.1
+ */
+public class Greetingcontroller {
+}
